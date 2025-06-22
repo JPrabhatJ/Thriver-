d339bef63f4cbe59cc135275bc7bee04c46c302a
Prabhat Jha <jhaprabhat268@gmail.com>
2025-06-22 12:19:15 +0530

    Accordion Changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -137,7 +137,7 @@ useEffect(() => {
       </div>
       <div className="box-back">
         <h3>Preparation Insights</h3>
-        <p>Anoop utilized our online-only plan. Regular doubt-clearing sessions, pre-recorded content, and speed tests helped him secure his dream role at IRDAI.</p>
+        <p>Anoop utilized our online study plan. Regular doubt-clearing sessions, pre-recorded content, and speed tests helped him secure his dream role at IRDAI.</p>
       </div>
     </div>
   </div>
